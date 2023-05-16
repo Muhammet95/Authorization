9ae0cd024e9b4e543b3b7f586f3b751b6b15f860
Muhammet95 <go.suits.3@gmail.com>
2023-05-17 00:42:42 +0300

    modified

--- index.php
+++ index.php
@@ -9,25 +9,30 @@ header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['PATH_INFO'] === '/auth/me') {
-    $auth = new AuthControllerAlias();
-    echo json_encode(['auth' => $auth->me()]);
+try {
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['PATH_INFO'] === '/auth/me') {
+        $auth = new AuthControllerAlias();
+        echo json_encode(['auth' => $auth->me()]);
+        return 0;
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['PATH_INFO'] === '/login') {
+        $auth = new AuthControllerAlias();
+        echo json_encode($auth->login());
+        return 0;
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['PATH_INFO'] === '/logout') {
+        $auth = new AuthControllerAlias();
+        echo json_encode(['logout' => $auth->logout()]);
+        return 0;
+    }
+
+    not_found();
+} catch (Exception $exception) {
+    echo json_encode(['status' => 'error', 'message' => $exception->getMessage()]);
     return 0;
 }
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['PATH_INFO'] === '/login') {
-    $auth = new AuthControllerAlias();
-    echo json_encode($auth->login());
-    return 0;
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['PATH_INFO'] === '/logout') {
-    $auth = new AuthControllerAlias();
-    echo json_encode(['logout' => $auth->logout()]);
-    return 0;
-}
-
-not_found();
 
 function not_found()
 {
